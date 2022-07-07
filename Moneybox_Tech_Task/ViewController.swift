5aca0fcadd1f852b076c325b3de137f202fa2f86
Raivis Olehno <raivis.olehno@outlook.com>
2022-07-07 17:34:24 +0100

    Initial Project Setup
    Created base project and added coocapods for future dependency initialization.

---
+++ Moneybox_Tech_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moneybox_Tech_Task
+//
+//  Created by Raivis on 07/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Moneybox_Tech_Task/Pods-Moneybox_Tech_Task-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Moneybox_Tech_Task : NSObject
+@end
+@implementation PodsDummy_Pods_Moneybox_Tech_Task
+@end
